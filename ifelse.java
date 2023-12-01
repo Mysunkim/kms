9cc760965d4e65341a5d104c18d3a013b62e05ae
kms <gongo0207@gmail.com>
2023-12-01 11:05:31 +0900

    ifelse

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+public class ifelse {
+
+    public static void main(String[] args){
+
+        String name = "hasimoto";
+        int age = 10;
+        
+
+        if(age>20){
+            System.out.println("U can buy to tabaco");
+        }
+        else{
+            System.out.println("U cannot buy to tabaco");
+        }
+        
+    }
+    
+}
